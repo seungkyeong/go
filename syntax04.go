b26876af3a9b987698bc68ac7d5d494c44ef11bd
skyeong <skyeong1003@naver.com>
2021-09-18 19:37:53 +0900

    syntax04.go v0.4

--- syntax04.go
+++ syntax04.go
@@ -6,7 +6,7 @@ import (
  	"time"
  )
 
-//난수 추출된 수의 소수 판정 프로그램 v0.3
+//난수 추출된 수의 소수 판정 프로그램 v0.4
 func main() {
  	seed := time.Now().Unix()
  	rand.Seed(seed)
@@ -18,11 +18,11 @@ func main() {
  	for i := 2; i < number; i++ {
  		if number%i == 0 {
  			isPrime = false
- 			//count = count + 1
  		}
+ 		fmt.Print(i, " ")
  	}
 
- 	if isPrime == true {
+ 	if isPrime { //비교 연산자 제거
  		fmt.Println(number, "는(은) 소수입니다!")
  	} else {
  		fmt.Println(number, "는(은) 소수가 아닙니다!")
